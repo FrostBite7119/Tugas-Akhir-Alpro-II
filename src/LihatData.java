72bc0f1878f1687a136f45196362c2be59fab4d3
FrostBite7119 <handy7119@gmail.com>
2021-07-14 00:36:13 +0700

    NxK fix

--- src/LihatData.java
+++ src/LihatData.java
@@ -175,7 +175,7 @@ public class LihatData extends javax.swing.JFrame {
                     }
                     data[5] = rs.getString("nilai");
                     double nxk = Double.parseDouble(rs.getString("nilai")) * Double.parseDouble(rs.getString("sks"));
-                    data[6] = String.format(".%1f", nxk);
+                    data[6] = String.format("%.1f", nxk);
                     model.addRow(data);
                     tabeljadwal.setModel(model);
                     no++;
